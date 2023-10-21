b7fd08e6ad53618100f6460fffa2be9dbea6d2c6
Paddy <ohanlon.paddy@gmail.com>
2023-10-21 14:36:41 +0200

    Add debug env vars

--- app/services/rethinkid.ts
+++ app/services/rethinkid.ts
@@ -1,6 +1,9 @@
 import { RethinkID } from "@rethinkid/rethinkid-js-sdk";
 import type { Options } from "@rethinkid/rethinkid-js-sdk";
 
+console.log("App ID", process.env.NEXT_PUBLIC_RETHINKID_APP_ID);
+console.log("Redirect URI", process.env.NEXT_PUBLIC_RETHINKID_REDIRECT_URI);
+
 const options: Options = {
   appId: process.env.NEXT_PUBLIC_RETHINKID_APP_ID || "",
   loginRedirectUri: process.env.NEXT_PUBLIC_RETHINKID_REDIRECT_URI || "",
