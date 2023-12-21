3d49f6e3393dff8128a4677a7c1924005a00bba7
Paddy <ohanlon.paddy@gmail.com>
2023-12-21 11:37:09 +0100

    Update SDK (no debugging), remove debugging

--- app/import/importPayee.ts
+++ app/import/importPayee.ts
@@ -8,35 +8,35 @@ export async function importPayee(
   const name = csvRow["Payee Name"];
   if (!name) return;
 
-  console.log("payee name", name);
+  // console.log("payee name", name);
 
   // Create unsaved doc
   const unsavedPayee: UnsavedPayee = { name, budgetId };
 
-  console.log("unsavedPayee", unsavedPayee);
+  // console.log("unsavedPayee", unsavedPayee);
 
   // Check doesn't exist
   let existingPayees = null;
 
-  console.log("existingPayees", existingPayees);
+  // console.log("existingPayees", existingPayees);
 
   try {
     existingPayees = await payeesCollection.getAll({ name, budgetId });
   } catch (error) {
-    console.log("error existingPayees getAll", error);
+    // console.log("error existingPayees getAll", error);
   }
 
-  console.log("existingPayees", existingPayees);
+  // console.log("existingPayees", existingPayees);
 
   if (existingPayees && existingPayees.length > 0) {
-    console.log("payee exists, return");
+    // console.log("payee exists, return");
     return;
   }
 
-  console.log("insert payee");
+  // console.log("insert payee");
 
   // Insert
   const res = await payeesCollection.insertOne(unsavedPayee);
 
-  console.log("inserted payee. res:", res);
+  // console.log("inserted payee. res:", res);
 }
